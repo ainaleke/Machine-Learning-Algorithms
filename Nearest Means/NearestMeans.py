8771a26ec9dd8685e5e7edeacec87e603833f9d0
Oluwaleke Aina <ainaleke@gmail.com>
2016-11-28 18:25:31 -0500

    Mean from all label points done...Classification done

--- Nearest Means/NearestMeans.py
+++ Nearest Means/NearestMeans.py
@@ -28,27 +28,47 @@ if(DEBUG):
 
 ###READ BREAST_CANCER LABELS
 labelfile=sys.argv[2]
-train_labels_dict={}
-n=[]
-for i in range(0,len(labelfile),1):
-    with open(labelfile) as f:
-        for line in f:
-            a=line.split()
-            training_labels[int (a[1])]=int (a[0])
-            #n=[0 , 1]
-            n[int (a[0])]+=1
+training_labels={}
+n=[0,0]
+#for i in range(0,len(labelfile),1):
+with open(labelfile) as f:
+    for line in f:
+        a=line.split()
+        training_labels[int (a[1])]=int (a[0])
+        #n=[0 , 1]
+        print(int(a[0]))
+        n[int (a[0])]+=1
 
 if(DEBUG):
     for i in range(0,len(n),1):
         print(n[i])
     for x in training_labels:
-        for y in training_labels[x]:
-            print(training_labels[x][y])
+        #for y in training_labels[x]:
+            print(training_labels[x])
 
 ##Calculate Means for each label indentifier either 0 or 1
 mean0=[]
 mean1=[]
 
-for i in range(0,rows,1):
-    mean0.append(0)
+for i in range(0,cols,1):
+    mean0.append(0.0)
+    mean1.append(0.0)
 
+for i in range(0,rows,1):
+    if(training_labels[i]!=None and training_labels[i]==0):
+        for j in range(0,cols,1):
+            mean0[j]+=data[i][j]
+    elif(training_labels[i]!=None and training_labels[i]==1):
+        for k in range(0,cols,1):
+            mean1[k]+=data[i][k]
+
+for p in range(0,cols,1):
+    mean0[p]/=n[0]
+    mean1[p]/n[1]
+print("Mean values with 0 as labels")
+print(mean0)
+
+print("Mean values with 1 as labels")
+print(mean1)
+
+#Calculate the distance of mean to each test point
