a5c9016efbd11e37c4ed790f26a0b00e0db98360
Oluwaleke Aina <ainaleke@gmail.com>
2016-11-28 16:09:44 -0500

    Updates

--- Nearest Means/NearestMeans.py
+++ Nearest Means/NearestMeans.py
@@ -34,5 +34,21 @@ for i in range(0,len(labelfile),1):
     with open(labelfile) as f:
         for line in f:
             a=line.split()
+            training_labels[int (a[1])]=int (a[0])
+            #n=[0 , 1]
+            n[int (a[0])]+=1
 
+if(DEBUG):
+    for i in range(0,len(n),1):
+        print(n[i])
+    for x in training_labels:
+        for y in training_labels[x]:
+            print(training_labels[x][y])
+
+##Calculate Means for each label indentifier either 0 or 1
+mean0=[]
+mean1=[]
+
+for i in range(0,rows,1):
+    mean0.append(0)
 
