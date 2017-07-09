da669cd86a37efbd24c5de9cbe9528336b5c2ecd
Oluwaleke Aina <ainaleke@gmail.com>
2017-07-08 22:59:48 -0400

    Added function to get priors or class probabilities

--- EmailSpamDetectionNaiveBayes/Spam_Email_Detection_NaiveBayes.py
+++ EmailSpamDetectionNaiveBayes/Spam_Email_Detection_NaiveBayes.py
@@ -4,7 +4,7 @@ from os.path import join, dirname
 from nltk.corpus import names
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
-
+from collections import defaultdict
 
 def letters_only(astr):
     return astr.isalpha()
@@ -19,23 +19,51 @@ file_path = 'enron1/spam'
 for filename in glob.glob(join(file_path, '*.txt')):
     with open(filename, 'r') as infile:
         emails.append(infile.read())
-        labels.append(1)
+        labels.append(1) #Label 1 represents a spam email
 
 file_path = 'enron1/ham'
 for filename in glob.glob(join(file_path, '*.txt')):
     with open(filename, 'r') as infile:
         emails.append((infile.read()))
-        labels.append(0)
+        labels.append(0) #Label 0 represents a legitimate email
 
-print len(emails)
-print len(labels)
 
-# Now we clean all the emails to remove
-# Number and punctuation removal
-# Human name removal(optional)
-# Stop words removal
-# Lemmatization
+"""
+Calculate Prior/ probability of the classes i.e. P(Spam) and P(Not Spam) and place results in
+dictionary
+"""
 
+def get_class_prob(labels):
+    """
+    :param labels: grouped sample indicies by class -> 0 or 1
+
+    Get each label which is either 0 or 1 and generate a dictionary with their indices from labels
+    :return: label_index = {0:[1500, 1501, 1502, 1503, 1504, 1505...], 1:[1,2,3,4,5,6...]},
+    """
+    label_index = defaultdict(list)
+
+    for index, label in enumerate(labels):
+        label_index[label].append(index)
+
+    prior = {}
+    for class_, index in label_index.iteritems():
+        print len(index)
+        #prior[class_] = len(index)
+
+    #Get total count of all labels in the list
+    #total_label_count = sum(label_index.values())
+
+get_class_prob(labels)
+
+
+"""
+ Now we clean all the emails to remove
+ Number and punctuation removal
+ Human name removal(optional)
+ Stop words removal
+ Lemmatization
+
+"""
 def clean_words(email_list):
     cleaned_emails = []
     temp = []
@@ -47,11 +75,30 @@ def clean_words(email_list):
         cleaned_emails.append(str)
     return cleaned_emails
 
+
+'''
+def clean_words(email_list):
+    cleaned_emails = []
+    for email in email_list:
+        #cleaned_emails.append(
+        str = ' '.join([lemmatizer.lemmatize(word.lower())
+        for word in email.split()
+        if letters_only(word) and word not in all_names])
+        print str
+        cleaned_emails.append(str)
+    return cleaned_emails
+'''
+
+
+
 if __name__ == "__main__":
-    # here we consider the 500 most frequent words and remove or eliminate all stop words like to, is etc.
+    # here we consider the 500 most frequent words and remove or elimin
     cv=CountVectorizer(stop_words="english", max_features=500)
     cleaned_email_list = clean_words(emails)
     print len(cleaned_email_list[0])
     transformed_emails = cv.fit_transform(cleaned_email_list)
 
-    print transformed_emails[0]
+    #print transformed_emails[0]
+
+    feature_names = cv.get_feature_names()
+    #print lemmatizer.lemmatize(feature_names[125])
